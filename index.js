3628aef71612754614c6edb5ddd7bbb9e1a4d067
Yuichi Saito <munisystem@gmail.com>
2017-05-11 22:02:21 +0900

    Add query normalizer

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const pgn = require('./build/Release/pg-query');
+const fs = require('fs');
 
-console.log(pgn.normalize("woohoo"));
+console.log(pgn.normalize(data).replace(/\?(, \?)+/g, "?"));
--- normalize.cc
+++ normalize.cc
@@ -14,7 +14,13 @@ NAN_METHOD(normalize) {
   if (info.Length() < 1) {
     return Nan::ThrowError("parameter must be provided.");
   }
-  Nan::Utf8String str(info[0]);
 
-  info.GetReturnValue().Set(Nan::To<v8::String>(info[0]).ToLocalChecked());
+  Nan::Utf8String query(info[0]);
+  PgQueryNormalizeResult result = pg_query_normalize(*query);
+
+  v8::Local<v8::String> normalized = Nan::New(result.normalized_query).ToLocalChecked();
+  printf("%s\n", "segfo");
+  pg_query_free_normalize_result(result);
+
+  info.GetReturnValue().Set(normalized);
 }
