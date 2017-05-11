e1c783dd4add40e2c84b4bb5bfab82088b383991
Yuichi Saito <munisystem@gmail.com>
2017-05-11 22:10:23 +0900

    Exporse this module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 const pgn = require('./build/Release/pg-query');
-const fs = require('fs');
 
-console.log(pgn.normalize(data).replace(/\?(, \?)+/g, "?"));
+module.exports = function(data) {
+  return pgn.normalize(data).replace(/\?(, \?)+/g, "?");
+}
