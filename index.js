de28544d9fd4037e9dc1df56d8c0bfc736c30553
Yuichi Saito <munisystem@gmail.com>
2017-05-12 15:35:17 +0900

    Add error handling

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 const pgn = require('./build/Release/pg-query');
 
 module.exports = function(data) {
-  return pgn.normalize(data).replace(/\?(, \?)+/g, "?");
+  try {
+    const normalized = pgn.normalize(data)
+    return normalized.replace(/\?(, \?)+/g, "?");
+  } catch(e) {
+    return ""
+  }
 }
--- normalize.cc
+++ normalize.cc
@@ -12,11 +12,16 @@ NAN_METHOD(normalize) {
   initialize();
 
   if (info.Length() < 1) {
-    return Nan::ThrowError("parameter must be provided.");
+    return Nan::ThrowError("query must be provided.");
   }
 
   Nan::Utf8String query(info[0]);
   PgQueryNormalizeResult result = pg_query_normalize(*query);
+
+  if (result.error) {
+    v8::Local<v8::String> message = Nan::New(result.error->message).ToLocalChecked();
+    return Nan::ThrowError(message);
+  }
 
   v8::Local<v8::String> normalized = Nan::New(result.normalized_query).ToLocalChecked();
   pg_query_free_normalize_result(result);
